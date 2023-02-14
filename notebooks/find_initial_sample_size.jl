b7879b019ad9a694536c5cb271a96ce8dc3ae6ad
Seth Axen <seth@sethaxen.com>
2023-02-14 15:39:40 +0100

    Support tuning delta

--- notebooks/find_initial_sample_size.jl
+++ notebooks/find_initial_sample_size.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.20
+# v0.19.22
 
 using Markdown
 using InteractiveUtils
@@ -25,8 +25,11 @@ end
 begin
     nruns = 10
     nchains = 4
-    ndraws = 1_000
+    initial_ndraws = 1_000
+    max_ndraws = 10_000
     target_ess_per_chain = 1_000
+    initial_delta = 0.8
+    max_delta = 0.99
     out_file = "hmc_config.json"
 end;
 
@@ -34,42 +37,74 @@ end;
 pdb = database()
 
 # ╔═╡ 7746714a-d1f6-46d3-8383-d2fdfa2389ce
-posterior_names = [
-    "arma-arma11", "eight_schools-eight_schools_centered", "diamonds-diamonds"
-]
+posterior_seeds = [
+    "arma-arma11" => 50872,
+    "eight_schools-eight_schools_centered" => 33701,
+    "diamonds-diamonds" => 18730,
+];
 
 # ╔═╡ 747bb969-a960-4b6e-bace-2062e4d0b2a7
 target_ndraws = let
-    rng = Random.seed!(87992)
-    target_ndraws = Dict()
-    @progress for name in posterior_names
-        post = posterior(pdb, name)
-        prob = StanProblem(post; nan_on_error=true, make_args=["STAN_THREADS=true"])
+    @progress for (name, seed) in posterior_seeds
+        rng = Random.seed!(seed)
+        path = name
+        post = posterior(pdb, path)
+        prob = StanProblem(
+            post, name; nan_on_error=true, force=true, make_args=["STAN_THREADS=true"]
+        )
         ess_values = Vector{Float64}[]
-        @progress for _ in 1:nruns
-            idata = sample_dynamichmc(prob, ndraws, nchains; rng)
-            y = idata.posterior.x
-            x = mapslices(idata.posterior.x; dims=3) do xi
-                param_constrain(prob.model, vec(xi))
+        δ = initial_delta
+        ndraws = initial_ndraws
+        nruns_finished = 0
+        ProgressLogging.progress() do id
+            while nruns_finished < nruns
+                @info "starting" progress = (nruns_finished + 1) / nruns _id = id
+                warmup_stages = default_warmup_stages(;
+                    stepsize_adaptation=DualAveraging(; δ)
+                )
+                idata = sample_dynamichmc(prob, ndraws, nchains; rng, warmup_stages)
+                y = idata.posterior.x
+                x = mapslices(idata.posterior.x; dims=3) do xi
+                    param_constrain(prob.model, vec(xi))
+                end
+                S, R = ess_rhat(x)
+                frac_div = mean(idata.sample_stats.diverging)
+                restart = false
+                if (frac_div > 0.01) && (δ < max_delta)
+                    δ = min(max_delta, δ + (1 - δ) / 2)
+                    @warn "increasing δ to $δ (frac_div: $frac_div)"
+                    restart = true
+                elseif (any(<(100 * nchains), S) || any(>(1.01), R)) &&
+                    (ndraws < max_ndraws)
+                    ndraws = min(ndraws * 2, max_ndraws)
+                    @warn "increasing ndraws to $ndraws (ess=$S, rhat=$R)"
+                    restart = true
+                end
+                if restart
+                    nruns_finished = 0
+                    deleteat!(ess_values, eachindex(ess_values))
+                else
+                    push!(ess_values, S)
+                    nruns_finished += 1
+                end
             end
-            S, _ = ess_rhat(x)
-            push!(ess_values, S)
         end
         mean_ess_vals = mean(ess_values)
-        ndraws_new = @.(max(ndraws, Int(cld(mean_ess_vals * target_ess_per_chain, ndraws))))
-        target_ndraws[name] = Dict(
+        ndraws_new = @. clamp(
+            div(mean_ess_vals * target_ess_per_chain, ndraws),
+            initial_ndraws,
+            max_ndraws,
+        )
+        config = Dict(
             "nchains" => nchains,
             "ndraws" => Int(round(maximum(ndraws_new), RoundUp; digits=-2)),
+            "delta" => δ,
         )
-    end
-    target_ndraws
-end
-
-# ╔═╡ 8275284a-e96e-4ec8-bebf-20c0a8388e9c
-for (name, config) in pairs(target_ndraws)
-    isdir(name) || mkdir(name)
-    open(joinpath(name, out_file), "w") do io
-        JSON3.write(io, config)
+        @info "chosen config for $name: $config"
+        isdir(name) || mkdir(name)
+        open(joinpath(name, out_file), "w") do io
+            JSON3.write(io, config)
+        end
     end
 end
 
@@ -79,4 +114,3 @@ end
 # ╠═6ee67094-215b-491e-bb8a-bcd3ef12b9c6
 # ╠═7746714a-d1f6-46d3-8383-d2fdfa2389ce
 # ╠═747bb969-a960-4b6e-bace-2062e4d0b2a7
-# ╠═8275284a-e96e-4ec8-bebf-20c0a8388e9c
